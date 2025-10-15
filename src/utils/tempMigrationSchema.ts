3e2ba68765e073d3c348733c06f89c99e6767805
Softjey <misivsvatoslav@gmail.com>
2025-10-15 10:57:48 +0200

    refactor(tempMigrationSchema): format code and replace comments by js docs

--- src/utils/tempMigrationSchema.ts
+++ src/utils/tempMigrationSchema.ts
@@ -1,26 +1,37 @@
 import fs from "fs";
 
-import { parsePrismaSchema, PrismaSchema, SchemaDeclaration, Config, formatAst } from "@loancrate/prisma-schema-parser";
+import {
+  parsePrismaSchema,
+  PrismaSchema,
+  SchemaDeclaration,
+  Config,
+  formatAst,
+} from "@loancrate/prisma-schema-parser";
 import { DataSourceConfig } from "../services/DB";
-import { prismaSqliteURLToFilePath } from "./readDataSourceConfig";
+import { prismaSqliteURLToFilePath } from "./prismaSqliteURLToFilePath";
 import { ConfigSchema } from "../config/config.type";
 
-
 function isNonClientGenerator(decl: SchemaDeclaration): boolean {
-  return decl.kind === "generator" && !(decl.members?.some(member => 
-    member.kind === "config" &&
-    member.name?.value === "provider" && 
-    member.value?.kind === "literal" && 
-    member.value?.value === "prisma-client-js"
-  ));
+  return (
+    decl.kind === "generator" &&
+    !decl.members?.some(
+      (member) =>
+        member.kind === "config" &&
+        member.name?.value === "provider" &&
+        member.value?.kind === "literal" &&
+        member.value?.value === "prisma-client-js",
+    )
+  );
 }
 
-/// Updates the client generator block to set the output path
-/// to the specified path. Removes any other generator blocks.
+/**
+ * Updates the client generator block to set the output path
+ * to the specified path. Removes any other generator blocks.
+ */
 function updateGenerator(ast: PrismaSchema, clientOutputPath: string): PrismaSchema {
   let astCopy = structuredClone(ast);
-  astCopy.declarations = astCopy.declarations.filter(decl => !isNonClientGenerator(decl));
-  const generators = astCopy.declarations.filter(decl => decl.kind === "generator");
+  astCopy.declarations = astCopy.declarations.filter((decl) => !isNonClientGenerator(decl));
+  const generators = astCopy.declarations.filter((decl) => decl.kind === "generator");
   if (generators.length !== 1) {
     throw new Error("The schema must contain exactly one generator block for prisma-client-js.");
   }
@@ -30,9 +41,8 @@ function updateGenerator(ast: PrismaSchema, clientOutputPath: string): PrismaSch
     throw new Error("The generator block must have a members array.");
   }
 
-  let generatorOutputAttribute = generator.members.find(attr =>
-    attr.kind === "config" &&
-    attr.name?.value === "output"
+  let generatorOutputAttribute = generator.members.find(
+    (attr) => attr.kind === "config" && attr.name?.value === "output",
   ) as Config | undefined;
   if (!generatorOutputAttribute) {
     throw new Error("The generator block is missing an output attribute.");
@@ -42,17 +52,19 @@ function updateGenerator(ast: PrismaSchema, clientOutputPath: string): PrismaSch
   return astCopy;
 }
 
-/// Updates the datasource block so that in case of SQLite,
-/// the file path in the URL is absolute (not relative).
-/// This is needed because the schema copied to the migrations
-/// folder would have a different relative path.
+/**
+ * Updates the datasource block so that in case of SQLite,
+ * the file path in the URL is absolute (not relative).
+ * This is needed because the schema copied to the migrations
+ * folder would have a different relative path.
+ */
 function updateDatasource(
   ast: PrismaSchema,
   dataSource: DataSourceConfig,
   config: ConfigSchema,
 ): PrismaSchema {
   let astCopy = structuredClone(ast);
-  const dataSources = astCopy.declarations.filter(decl => decl.kind === "datasource");
+  const dataSources = astCopy.declarations.filter((decl) => decl.kind === "datasource");
   if (dataSources.length !== 1) {
     throw new Error("The schema must contain exactly one datasource");
   }
@@ -61,9 +73,8 @@ function updateDatasource(
     throw new Error("The datasource block must have a members array.");
   }
 
-  let dataSourceUrlAttribute = dataSourceDecl.members.find(attr =>
-    attr.kind === "config" &&
-    attr.name?.value === "url"
+  let dataSourceUrlAttribute = dataSourceDecl.members.find(
+    (attr) => attr.kind === "config" && attr.name?.value === "url",
   ) as Config | undefined;
 
   if (!dataSourceUrlAttribute) {
@@ -80,13 +91,13 @@ function updateDatasource(
   return astCopy;
 }
 
-/// Creates a temporary Prisma schema file for generating
-/// the client for a migration. The schema is based on the
-/// source schema file, but with updated generator and datasource
-/// blocks.
+/**
+ * Creates a temporary Prisma schema file for generating the client for a migration.
+ * The schema is based on the source schema file, but with updated generator and datasource blocks.
+ */
 export function createTempSchema(
   srcPrismaSchemaPath: string,
-  clientOutputPath: string, 
+  clientOutputPath: string,
   dataSource: DataSourceConfig,
   outPrismaSchemaPath: string,
   config: ConfigSchema,
