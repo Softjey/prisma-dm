5812e90e8fbd972ae885f82f33e6f2886c6a07d6
Softjey <misivsvatoslav@gmail.com>
2025-10-15 10:58:18 +0200

    feat: add isSupportedDatasourceProvider utility

---
+++ src/utils/isSupportedDatasourceProvider.ts
@@ -0,0 +1,8 @@
+export const SUPPORTED_DATASOURCE_PROVIDERS = ["postgresql", "sqlite"] as const;
+export type SupportedDatasourceProvider = (typeof SUPPORTED_DATASOURCE_PROVIDERS)[number];
+
+export default function isSupportedDatasourceProvider(
+  provider: string,
+): provider is SupportedDatasourceProvider {
+  return SUPPORTED_DATASOURCE_PROVIDERS.includes(provider as SupportedDatasourceProvider);
+}
